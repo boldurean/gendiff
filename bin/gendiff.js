4077af2304ca5d0695314aec39367c0da3174305
vsbdn <v.boldurean@gmail.com>
2021-01-17 14:47:26 +0000

    added -f flag/description to hepler added file arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,5 +4,8 @@ import program from 'commander';
 program
   .version('0.0.1')
   .description('An application for pizza ordering')
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
 
 program.parse(process.argv);
