b5d2e116bb7a7caf2be4054c99a110d6cd96b19d
vsbdn <v.boldurean@gmail.com>
2021-01-26 15:35:04 +0000

    tests refactoring

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -7,19 +7,18 @@ const getFixturePath = (filename) => join(process.cwd(), '__fixtures__', filenam
 const expectedStylish = parse(getFixturePath('expectedStylish.txt'));
 const expectedPlain = parse(getFixturePath('expectedPlain.txt'));
 
-test.each([
-  ['file1.json', 'file2.json', expectedStylish],
-  ['file1.yml', 'file2.json', expectedStylish],
-  ['file1.json', 'file2.yml', expectedStylish],
-  ['file1.yml', 'file2.yml', expectedStylish],
-])('checks stylish output %s %s', (a, b, expected) => {
-  expect(genDiff(getFixturePath(a), getFixturePath(b), 'stylish')).toEqual(expected);
-});
-test.each([
-  ['file1.json', 'file2.json', expectedPlain],
-  ['file1.yml', 'file2.yml', expectedPlain],
-  ['file1.yml', 'file2.json', expectedPlain],
-  ['file1.json', 'file2.yml', expectedPlain],
-])('checks plain output %s %s', (a, b, expected) => {
-  expect(genDiff(getFixturePath(a), getFixturePath(b), 'plain')).toEqual(expected);
+const filePairs = [
+  ['file1.json', 'file2.json'],
+  ['file1.yml', 'file2.json'],
+  ['file1.json', 'file2.yml'],
+  ['file1.yml', 'file2.yml'],
+];
+
+describe.each(filePairs)('compairs two files %s and %s', (a, b) => {
+  test('Styilish', () => {
+    expect(genDiff(getFixturePath(a), getFixturePath(b), 'stylish')).toEqual(expectedStylish);
+  });
+  test('Plain', () => {
+    expect(genDiff(getFixturePath(a), getFixturePath(b), 'plain')).toEqual(expectedPlain);
+  });
 });
